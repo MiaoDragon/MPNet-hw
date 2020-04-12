2f8996096e1d5e28a7954da78a049df6a2dbef10
MiaoDragon <innocentmyl@gmail.com>
2020-04-11 21:52:51 -0700

    Update mpnet_train_s2d.sh

--- exp/mpnet_train_s2d.sh
+++ exp/mpnet_train_s2d.sh
@@ -1,6 +1,6 @@
 cd ..
 python3 mpnet_train.py --model_path ./models/ \
---no_env 100 --no_motion_paths 400 --learning_rate 0.01 --num_epochs 100 \
+--no_env 100 --no_motion_paths 4000 --learning_rate 0.01 --num_epochs 100 --batch_size 100 \
 --device 3  --start_epoch 0 --data_path data/ --world_size 20 --env_type s2d \
 --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2
 cd exp
