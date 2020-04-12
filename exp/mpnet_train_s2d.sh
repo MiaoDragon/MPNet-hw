394f26df9f67d4913f9d752e280d8ccc984bfa4b
MiaoDragon <innocentmyl@gmail.com>
2020-04-11 21:49:36 -0700

    a

--- exp/mpnet_train_s2d.sh
+++ exp/mpnet_train_s2d.sh
@@ -1,7 +1,7 @@
 cd ..
 python3 mpnet_train.py --model_path ./models/ \
---no_env 10 --no_motion_paths 400 --grad_step 1 --learning_rate 0.01 \
---num_epochs 1 --device 3  --start_epoch 0 --data_path data/ --world_size 20 --env_type s2d \
+--no_env 10 --no_motion_paths 400 --learning_rate 0.01 --num_epochs 1 \
+--device 3  --start_epoch 0 --data_path data/ --world_size 20 --env_type s2d \
 --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2
 cd exp
 
