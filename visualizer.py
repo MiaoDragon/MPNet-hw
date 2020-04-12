092077717e070f55163857527b66b8d7c9f293f6
MiaoDragon <innocentmyl@gmail.com>
2020-04-11 21:12:18 -0700

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -26,7 +26,7 @@ def main(args):
         path_x.append(path[i][0])
         path_y.append(path[i][1])
 
-    plt.plot(path_x, path_y, c='red')
+    plt.plot(path_x, path_y, c='ro')
 
     plt.show()
 
